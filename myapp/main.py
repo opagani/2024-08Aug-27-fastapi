829daef1099fff1d0a435979c8d667de7967d78b
Reuven Lerner <reuven@lerner.co.il>
2024-08-27 19:21:50 +0300

    gitautosync update 44

--- myapp/main.py
+++ myapp/main.py
@@ -12,3 +12,10 @@ app = FastAPI()
 @app.get('/hello/{name}')   # curly braces mean: capture this part of the URL, and turn into a function parameter
 async def hello(name):      # we'll get name defined based on the URL, thanks to FastAPI's assignment
     return {'message':f'Hello out there, {name}!'}
+
+@app.get('/greeting/{name}')  # we'll have one path parameter, name
+async def hello(name, x, y):  # we have two query parameters, x and y
+    return {'message':f'Hello out there, {name}!',
+            'x':x,
+            'y':y,
+            'x+y':x+y}
