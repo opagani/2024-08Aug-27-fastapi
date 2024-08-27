55c7c37ec5bfc9413845c0b1e62addd20a388124
Reuven Lerner <reuven@lerner.co.il>
2024-08-27 18:36:55 +0300

    gitautosync update 23

--- myapp/main.py
+++ myapp/main.py
@@ -3,3 +3,8 @@ from fastapi import FastAPI
 
 # create a new application instance
 app = FastAPI()
+
+# let's define a new API "endpoint"
+# we do that in two parts:
+# (1) we use a decorator, @app.get('/the_url') to associate the function with a URL
+# (2) we use a function to indicate what functionality we want
